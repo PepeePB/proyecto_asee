849277d8e41222f0a4c0d75fa27fcbc9b039bc54
PepeePB <116971033+PepeePB@users.noreply.github.com>
2025-12-18 01:53:34 +0100

    Add files via upload

---
+++ proyecto-asee-25-26-gb03-eureka-microservice/src/test/java/es/unex/aos/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.unex.aos.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
